af38b3cf6352ae20e2e49565d127d32bfd713ac9
Nina Petropoulos <petropoulos.nina@gmail.com>
2016-01-14 18:31:57 -0500

    slide out navigation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+  // Mobile Navigation
+  $('.mobile-nav-toggle').click(function(){
+
+    $('.slideNav-container').toggleClass('reveal');
+    $('.wrap').toggleClass('reveal');
+
+  });
+
   // when user hovers over col-3 highlight
   $('.project').mouseenter(function(){
     $(this).addClass('highlight');
