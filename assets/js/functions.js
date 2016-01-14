9a180a2ff5a66b6a47b067566d849f925e005a82
Nina Petropoulos <petropoulos.nina@gmail.com>
2016-01-14 17:28:10 -0500

    basic responsiveness

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
 
+  // when user hovers over col-3 highlight
+  $('.project').mouseenter(function(){
+    $(this).addClass('highlight');
+  });
+  $('.project').mouseleave(function(){
+    $(this).removeClass('highlight');
+  });
+});
